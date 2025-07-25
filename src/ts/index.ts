cddab8b331ceb376c7d3ec382ea16e4e751a3c68
SIGENPOWER100760 <fanghairong@sigenergy.com>
2025-07-25 19:24:16 +0800

    1

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,36 @@
 
+
+//-----------------------------------------------1：一条直线
+/*
 const cnv: any = document.getElementById('c');
 const cxt = cnv.getContext('2d');
-//起点坐标 (x, y)
 cxt.moveTo(100, 100);
-//终点坐标 (x, y)
 cxt.lineTo(200, 100);
-//将起点和终点连接起来
+cxt.stroke();
+*/
+
+//-----------------------------------------------2：多条直线
+/*
+const cnv: any = document.getElementById('c');
+const cxt = cnv.getContext('2d');
+cxt.moveTo(20, 100);
+cxt.lineTo(200, 100);
+cxt.stroke();
+
+cxt.moveTo(20, 120.5);
+cxt.lineTo(200, 120.5);
+cxt.stroke();
+*/
+
+//-----------------------------------------------3：直线加颜色
+const cnv: any = document.getElementById('c');
+const cxt = cnv.getContext('2d');
+
+cxt.moveTo(50, 50);
+cxt.lineTo(200, 50);
+
+cxt.lineWidth = 20;
+cxt.strokeStyle = 'red';
+cxt.lineCap = 'round';
+
 cxt.stroke();
