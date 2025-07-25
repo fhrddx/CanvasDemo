eaee919649202ee31f0a3a9f57d95323716e4746
SIGENPOWER100760 <fanghairong@sigenergy.com>
2025-07-25 14:47:09 +0800

    fix

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,9 @@
-import GeoWorld from './GeoWorld/GeoWorld';
 
-const dom: HTMLElement = document.querySelector('#earth-canvas')!;
-
-new GeoWorld({
-  dom: dom
-})
+const cnv: any = document.getElementById('c');
+const cxt = cnv.getContext('2d');
+//起点坐标 (x, y)
+cxt.moveTo(100, 100);
+//终点坐标 (x, y)
+cxt.lineTo(200, 100);
+//将起点和终点连接起来
+cxt.stroke();
